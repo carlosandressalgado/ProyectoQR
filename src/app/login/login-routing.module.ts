28c02c5428db510f087c3f615531fb8a0bc40776
daniel15121512 <137961276+daniel15121512@users.noreply.github.com>
2024-10-27 13:17:34 -0300

    Revert "Implementación E404 + Guard"

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginPage } from './login.page';
-import { AngularFireAuthGuard } from '@angular/fire/auth-guard';
 
 const routes: Routes = [
   {
     path: '',
-    component: LoginPage, canActivate: [AngularFireAuthGuard]
+    component: LoginPage
   }
 ];
 
